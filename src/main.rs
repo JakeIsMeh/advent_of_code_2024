8272278b51d9007a5abb6af7fe7cd62fed0379fa
JakeIsMeh <jakeismeh1@gmail.com>
2024-12-11 11:09:00 +0800

    start project

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use std::{path::PathBuf, process::exit};
+
+use clap::{arg, value_parser, Command};
+
+mod day1;
+
+fn main() -> Result<(), std::io::Error> {
+    let matches = Command::new("AoC 2024") // requires `cargo` feature
+        .version("0.0")
+        .arg(
+            arg!(<day> "day's puzzle to run")
+                .value_parser(value_parser!(i32))
+                .required(true),
+        )
+        .arg(
+            arg!([part])
+                .value_parser(value_parser!(i32))
+                .default_value("1"),
+        )
+        .arg(
+            arg!(
+                -d --data <file> "Use a custom data input"
+            )
+            // We don't have syntax yet for optional options, so manually calling `required`
+            .required(false)
+            .value_parser(value_parser!(PathBuf)),
+        )
+        .get_matches();
+
+    let day = *(matches.get_one::<i32>("day").expect("day is missing"));
+
+    let part = *(matches.get_one::<i32>("part").expect("part is missing"));
+    let path: String;
+
+    if let Ok(Some(p)) = matches.try_get_one::<PathBuf>("file") {
+        path = p.to_string_lossy().to_string();
+    } else {
+        path = format!("./data/day{}.txt", day);
+    }
+
+    if !(..=2).contains(&part) {
+        println!(r"Part should fall within 0-2. (0 for both parts)\n");
+        exit(1);
+    }
+
+    match day {
+        _ => println!("Day not implemented."),
+    }
+
+    return Ok(());
+}
